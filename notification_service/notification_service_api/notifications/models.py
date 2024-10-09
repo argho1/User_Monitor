c33b74d5db848272cef667455fd1b4e2b88725fc
Argho Sinha <argho1@live.com>
2024-10-09 23:16:17 +0530

    first migration of UserNotificationPreferances

--- notification_service/notification_service_api/notifications/models.py
+++ notification_service/notification_service_api/notifications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class UserNotificationPreferance(models.Model):
+    user_id = models.IntegerField(unique=True)
+    email_enabled = models.BooleanField(default=True)
+    sms_enabled = models.BooleanField(default=True)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+
+
+
+
+
