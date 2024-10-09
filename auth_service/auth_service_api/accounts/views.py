cc6ecf4d05c4eb94831cd648628f93cef2a6ec4d
Argho Sinha <argho1@live.com>
2024-10-09 23:18:23 +0530

    adding response with cookies and commenting it out

--- auth_service/auth_service_api/accounts/views.py
+++ auth_service/auth_service_api/accounts/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .serializers import RegisterSerializer, LoginSerializer
 
@@ -25,10 +25,18 @@ class LoginView(generics.GenericAPIView):
 
         if user:
             refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh' : str(refresh),
+            # Create the Response instance first
+            response = Response({
+                'refresh': str(refresh),
                 'access': str(refresh.access_token),
             }, status=status.HTTP_200_OK)
+            ##COOKITE based auth if needed
+            ##Set the cookie on the Response instance
+            # response.set_cookie(key='jwt',
+            #                     value=str(refresh.access_token), 
+            #                     httponly=True)
+            return response
+        
         return Response({
             'detail':'Invalid Credentials'
         }, status=status.HTTP_401_UNAUTHORIZED)
@@ -37,5 +45,6 @@ class ValidateTokenView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        print(request.headers)
         return Response({'detail':'Token is valid'},
-                        status=status.status.HTTP_200_OK)
+                        status=status.HTTP_200_OK)
