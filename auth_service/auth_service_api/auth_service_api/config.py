69d7c159951f3b2b89daf82706c497c5215b302b
Argho Sinha <argho1@live.com>
2024-10-10 02:15:22 +0530

    pilot .env getter file

---
+++ auth_service/auth_service_api/auth_service_api/config.py
@@ -0,0 +1,7 @@
+import os
+
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
+RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))
+RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
+RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest')
+RABBITMQ_QUEUE = os.getenv('RABBITMQ_QUEUE', 'user_registered')
