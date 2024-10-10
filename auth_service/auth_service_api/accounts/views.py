9bfd63619bc1bdd799495a18b0bb3c889318cb3f
Argho Sinha <argho1@live.com>
2024-10-10 12:28:01 +0530

    Adding throttle_classes and phone_number

--- auth_service/auth_service_api/accounts/views.py
+++ auth_service/auth_service_api/accounts/views.py
@@ -1,18 +1,24 @@
 import logging
 from django.db import transaction
 from django.contrib.auth import authenticate
-from rest_framework.views import APIView
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.throttling import UserRateThrottle
 from .serializers import RegisterSerializer, LoginSerializer
 from auth_service_api.rabbitmq_publisher import RabbitMQPublisher
 
 logger = logging.getLogger(__name__)
 
+# To prevent brute-force or abuse
+class BurstRateThrottle(UserRateThrottle):
+    rate = '5/min'
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    throttle_classes = [BurstRateThrottle]
 
     @transaction.atomic # to ensure atomic db transactions
     def post(self, request, *args, **kwargs):
@@ -25,11 +31,16 @@ class RegisterView(generics.CreateAPIView):
                     'user_id' : user.id,
                     'email' : user.email,
                     'username' : user.username,
+                    'phone_number': user.phone_number,
                 }
                 publisher.publish(message)
                 publisher.close()
             except Exception as e:
                 logger.error(f"Error publishing to RabbitMQ: {e}")
+                # comment out below lines if publishing is not critical
+                transaction.set_rollback(True)
+                return Response({'detail': 'Registration failed. Please try again later.'}, 
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             return Response({'detail': 'User registered successfully'}, 
                             status=status.HTTP_201_CREATED)
         else:
@@ -43,7 +54,8 @@ class LoginView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user = authenticate(
             username = serializer.validated_data['username'],
-            password = serializer.validated_data['password']
+            password = serializer.validated_data['password'],
+            phone_number = serializer.validated_data['phone_number'],
         )
 
         if user:
