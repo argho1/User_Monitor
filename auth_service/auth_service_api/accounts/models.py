18dd1679644263b3ec46ee80ab3e3fa7c35a80ba
Argho Sinha <argho1@live.com>
2024-10-10 12:25:47 +0530

    adding phone_number to database and migrating

--- auth_service/auth_service_api/accounts/models.py
+++ auth_service/auth_service_api/accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     # For adding custom fields if required
-    pass
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(unique=True, max_length=15, null=True, blank=True)
 
     def __str__(self):
         return self.username
