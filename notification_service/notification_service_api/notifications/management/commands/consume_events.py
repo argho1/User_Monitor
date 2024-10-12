196d56069b797b580c7caf5caf0f3c8d79c3ee8a
Argho Sinha <argho1@live.com>
2024-10-12 14:14:24 +0530

    changing queue consuer to consume for 2 queus

--- notification_service/notification_service_api/notifications/management/commands/consume_events.py
+++ notification_service/notification_service_api/notifications/management/commands/consume_events.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.core.management.base import BaseCommand
 import pika
-from notifications.tasks import handle_user_registered_event
+from notifications.tasks import handle_report_generated_event, handle_user_registered_event
 
 class Command(BaseCommand):
     help = 'Consume message from RabbitMQ'
@@ -9,16 +9,46 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         channel = connection.channel()
+        
+        # Declare both queues
         channel.queue_declare(queue='user_registered', durable=True)
+        channel.queue_declare(queue='report_generated', durable=True)
 
-        def callback(ch, method, properties, body):
-                try:
-                    handle_user_registered_event.delay(body.decode())
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                except Exception as e:
-                    print(f'Error: {e}')
-                    ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+        # Callback for user_registered queue
+        def user_registered_callback(ch, method, properties, body):
+            try:
+                handle_user_registered_event.delay(body.decode())
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            except Exception as e:
+                print(f'Error in user_registered_callback: {e}')
+                ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
 
-        channel.basic_consume(queue='user_registered', on_message_callback=callback, auto_ack=False)
-        print(' [*] waiting for message. To exit press CTRL+C ')
+        # Callback for report_generated queue
+        def report_generated_callback(ch, method, properties, body):
+            try:
+                handle_report_generated_event.delay(body.decode())
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            except Exception as e:
+                print(f'Error in user_registered_callback: {e}')
+                ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+
+
+        channel.basic_consume(
+            queue='user_registered',
+            on_message_callback=user_registered_callback,
+            auto_ack=False,
+        )
+
+        channel.basic_consume(
+            queue='report_generated',
+            on_message_callback=report_generated_callback,
+            auto_ack=False,
+        )
+
+        print(' [*] Waiting for message. To exit press CTRL+C ')
         channel.start_consuming()
+
+
+
+
+
