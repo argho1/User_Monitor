4d18555e6368c85aa71d20edcf11b67fec9f0d04
Argho Sinha <argho1@live.com>
2024-10-12 14:13:16 +0530

    adding seperate queues

--- notification_service/notification_service_api/notification_service_api/celery.py
+++ notification_service/notification_service_api/notification_service_api/celery.py
@@ -1,6 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from kombu import Queue
+
+CELERY_QUEUES = (
+    Queue('user_registered', routing_key='user.registered'),
+    Queue('report_generated', routing_key='report.generated'),
+)
+
+CELERY_ROUTES = {
+    'notifications.tasks.handle_user_registered_event': {'queue': 'user_registered', 
+                                                         'routing_key': 'user.registered'},
+    'notifications.tasks.handle_report_generated_event': {'queue': 'report_generated', 
+                                                          'routing_key': 'report.generated'},
+}
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notification_service_api.settings')
