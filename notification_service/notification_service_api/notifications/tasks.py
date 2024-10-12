0904198c14ed18181f3e9ed4aab7650f3663a1ca
Argho Sinha <argho1@live.com>
2024-10-12 14:17:02 +0530

    adding seperate email tasks for report generation and user regeristeration

--- notification_service/notification_service_api/notifications/tasks.py
+++ notification_service/notification_service_api/notifications/tasks.py
@@ -1,9 +1,9 @@
 import json
 from django.template.loader import render_to_string
 from celery import shared_task
-from .utils import send_email, send_sms
+from .utils import get_report_file, send_email, send_sms
 
-@shared_task
+@shared_task(name='notifications.tasks.handle_user_registered_event')
 def handle_user_registered_event(message):
     event = json.loads(message)
     email = event.get('email')
@@ -26,3 +26,22 @@ def handle_user_registered_event(message):
     # phone_number = event.get('phone_number')
     # sms_content = f'Hello {username}, thank you for registering!'
     # send_sms(to_number=phone_number, message=sms_content)
+
+
+@shared_task(name='notifications.tasks.handle_report_generated_event')
+def handle_report_generated_event(message):
+    event = json.loads(message)
+    email = event.get('email')
+    report_id = event.get('report_id')
+    report_type = event.get('report_type')
+
+    report_content = get_report_file(report_id)
+    if not report_content:
+        return
+    
+    subject = f'Your {report_type} Report'
+    content = 'Please find your report attached.'
+
+    send_email(to_email=email, subject=subject, content=content, attachment=report_content)
+
+
