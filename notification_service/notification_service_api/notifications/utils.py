cd261783a623b4b45c86720fa48d86623f126f7c
Argho Sinha <argho1@live.com>
2024-10-12 14:17:44 +0530

    makeing send_email, handle attachments

--- notification_service/notification_service_api/notifications/utils.py
+++ notification_service/notification_service_api/notifications/utils.py
@@ -1,15 +1,29 @@
+import base64
+import requests
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
 from django.conf import settings
 from twilio.rest import Client
 
-def send_email(to_email, subject, content):
+def send_email(to_email, subject, content, attachment=None):
     message = Mail(
         from_email="sinhaargho@gmail.com",
         to_emails=to_email,
         subject=subject,
         plain_text_content=content,
     )
+
+    if attachment:
+        encoded_file = base64.b64encode(attachment).decode('utf-8')
+        attachment_file = Attachment(
+            FileContent(encoded_file),
+            FileName('report.pdf'),
+            FileType('application/pdf'),
+            Disposition('attachment')
+        )
+
+        message.attachment = attachment_file
+
     try:
         sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
         sg.send(message)
@@ -29,3 +43,21 @@ def send_sms(to_number, message):
         print(f"SMS send to {to_number}")
     except Exception as e:
         print(f'Error sending SMS: {e}')
+
+
+def get_report_file(report_id):
+    report_service_url = f'http://127.0.0.1:7000/reports/{report_id}/download/'
+
+    try:
+        response = requests.get(report_service_url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            print(f'Failed to fetch report : {response.status_code}')
+            return None
+    except Exception as e:
+        print(f'Error fetching report:{e}')
+        return None
+
+
+
