a2c27930a0faf251daed6804b72df2cfa578f852
Argho Sinha <argho1@live.com>
2024-10-12 14:20:47 +0530

    adding models and migrating

--- reporting_service/report_service_api/reports/models.py
+++ reporting_service/report_service_api/reports/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='reports/generated_reports/')
