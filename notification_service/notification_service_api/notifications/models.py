6e387af080f99c79b5f3e1b0e5653b5f2f3d2667
Argho Sinha <argho1@live.com>
2024-10-13 16:11:06 +0530

    removing notification_preferance from repots and migrating

--- notification_service/notification_service_api/notifications/models.py
+++ notification_service/notification_service_api/notifications/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-class UserNotificationPreferance(models.Model):
-    user_id = models.IntegerField(unique=True)
-    email_enabled = models.BooleanField(default=True)
-    sms_enabled = models.BooleanField(default=True)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=15)
-
-
-
 
