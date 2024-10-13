d90bcc19986794db6931814190c9665a7dbb1669
Argho Sinha <argho1@live.com>
2024-10-13 15:43:08 +0530

    added seperate queue to solve tasks overlapping issue

--- reporting_service/report_service_api/report_service_api/celery.py
+++ reporting_service/report_service_api/report_service_api/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from kombu import Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'report_service_api.settings')
 
@@ -10,3 +11,18 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.broker_connection_retry_on_startup = True
+
+
+# Define the queue for the report service
+app.conf.task_queues = (
+    Queue('report_tasks', routing_key='report.#'),
+)
+
+
+# Route the tasks for the report service to the correct queue
+app.conf.task_routes = {
+    'reports.tasks.generate_scheduled_report_n_send': {
+        'queue': 'report_tasks',
+        'routing_key': 'report.scheduled'
+    },
+}
