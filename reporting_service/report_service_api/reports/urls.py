fb1f442624d0b90b77eb4fb59840b2976b6755bb
Argho Sinha <argho1@live.com>
2024-10-13 15:44:50 +0530

    report/ now shows history at /reports/history

--- reporting_service/report_service_api/reports/urls.py
+++ reporting_service/report_service_api/reports/urls.py
@@ -3,7 +3,7 @@ from .views import ReportListView, ReportDetailView, GenerateReportView, ReportD
 
 urlpatterns = [
     path('reports/generate/', GenerateReportView.as_view(), name='generate-report'),
-    path('reports/', ReportListView.as_view(), name='report-list'),
+    path('reports/history/', ReportListView.as_view(), name='report-list'),
     path('reports/<int:pk>/', ReportDetailView.as_view(), name='report-detail'),
     path('reports/<int:pk>/download/', ReportDownloadView.as_view(), name='report-download'),
 ]
