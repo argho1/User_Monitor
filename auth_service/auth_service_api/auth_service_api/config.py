3d9bf6e04eab65f3151a50bfc5443077dc4191ea
Argho Sinha <argho1@live.com>
2024-10-14 08:23:04 +0530

    removing config.py, moding config to .env and settings

--- auth_service/auth_service_api/auth_service_api/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
-RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))
-RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
-RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest')
