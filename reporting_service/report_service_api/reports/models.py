6ae2664405a0c98fff04bfa474851afa687b9c78
Argho Sinha <argho1@live.com>
2024-10-14 16:18:59 +0530

    adding and commenting out code for auth view/delete

--- reporting_service/report_service_api/reports/models.py
+++ reporting_service/report_service_api/reports/models.py
@@ -4,3 +4,15 @@ class Report(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to='reports/generated_reports/')
+
+    # class Meta:
+    #     permissions = [
+    #         ('view_report', 'Can view report'),
+    #         ('delete_report', 'Can delete report'),
+    #     ]
+
+    def __str__(self):
+        return self.title
+
+
+
