d0690512f5df19685706fb3d42e03cebceed8d23
Argho Sinha <argho1@live.com>
2024-10-14 16:24:27 +0530

    authenticat with multiple fields logic, UNUSED

--- auth_service/auth_service_api/accounts/utils.py
+++ auth_service/auth_service_api/accounts/utils.py
@@ -1,35 +1,35 @@
 from .models import CustomUser
 
 
-def authenticate_with_multiple_fields(identifier, password):
-        """
-        Authenticate user using phone number, email, or username.
-        """
-        user = None
+# def authenticate_with_multiple_fields(identifier, password):
+#         """
+#         Authenticate user using phone number, email, or username.
+#         """
+#         user = None
 
-        # First, check if identifier is an email
-        if '@' in identifier:
-            try:
-                user = CustomUser.objects.get(email=identifier)
-            except CustomUser.DoesNotExist:
-                pass
+#         # First, check if identifier is an email
+#         if '@' in identifier:
+#             try:
+#                 user = CustomUser.objects.get(email=identifier)
+#             except CustomUser.DoesNotExist:
+#                 pass
         
-        # If not email, check if it is a phone number
-        if not user and identifier.isdigit():
-            try:
-                user = CustomUser.objects.get(phone_number=identifier)
-            except CustomUser.DoesNotExist:
-                pass
+#         # If not email, check if it is a phone number
+#         if not user and identifier.isdigit():
+#             try:
+#                 user = CustomUser.objects.get(phone_number=identifier)
+#             except CustomUser.DoesNotExist:
+#                 pass
         
-        # If not phone number, try it as a username
-        if not user:
-            try:
-                user = CustomUser.objects.get(username=identifier)
-            except CustomUser.DoesNotExist:
-                pass
+#         # If not phone number, try it as a username
+#         if not user:
+#             try:
+#                 user = CustomUser.objects.get(username=identifier)
+#             except CustomUser.DoesNotExist:
+#                 pass
         
-        # Authenticate user if found
-        if user and user.check_password(password):
-            return user
+#         # Authenticate user if found
+#         if user and user.check_password(password):
+#             return user
 
-        return None
+#         return None
